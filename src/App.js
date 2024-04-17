579f64dda4ee64c666feb58c3c360e451d1bb784
Raja Barnwal <rajabarnwal228@gmail.com>
2024-04-17 11:32:39 +0530

    Foodie-react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
